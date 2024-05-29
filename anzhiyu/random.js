da5ec4f0fa2e4d14ff84a29e1d6524606b347ac0
uek2233 <uek3442237157@gmail.com>
2024-05-29 19:31:50 +0800

    Site updated: 2024-05-29 19:31:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/aa11dae6.html","posts/aa11dae6.html","posts/4a17b156.html","posts/673b0eac.html","posts/81737f40.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html","posts/673b0eac.html","posts/aa11dae6.html","posts/aa11dae6.html","posts/81737f40.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
